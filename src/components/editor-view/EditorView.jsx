fbb53aaa5478abff0bbcc3ed97bf3db1a85a082e
Henry Caldwell <henrycaldwell2005@gmail.com>
2025-08-06 00:15:15 -0400

    feat(file-path-bar): add FilePathBar breadcrumb navigation
    Rendered clickable breadcrumb segments with arrows.

--- src/components/editor-view/EditorView.jsx
+++ src/components/editor-view/EditorView.jsx
@@ -1,10 +1,12 @@
 import "./EditorView.css";
+import FilePathBar from "./FilePathBar";
 import TabBar from "./TabBar";
 
 export default function EditorView() {
   return (
     <div className="editor-view">
       <TabBar />
+      <FilePathBar />
     </div>
   );
 }
---
+++ src/components/editor-view/FilePathBar.jsx
@@ -0,0 +1,34 @@
+import rightArrow from "../../assets/icons/right.svg";
+import "./FilePathBar.css";
+
+export default function FilePathBar() {
+  const segments = ["test", "another-test", "README.md"];
+
+  return (
+    <nav className="file-path-bar">
+      <ol className="file-path-bar__list">
+        {segments.map((seg, i) => {
+          const isLast = i === segments.length - 1;
+          return (
+            <li key={seg} className="file-path-bar__item">
+              <button
+                className="file-path-bar__button"
+                onClick={() => console.log(seg)}
+              >
+                <span className="file-path-bar__segment">{seg}</span>
+                {!isLast && (
+                  <img
+                    className="file-path-bar__arrow"
+                    src={rightArrow}
+                    alt=""
+                    aria-hidden="true"
+                  />
+                )}
+              </button>
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+}
